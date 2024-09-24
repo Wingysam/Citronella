cb4a19c2076e9d366f6c4ba4019b19d5db87aa8b
Wingy <git@wingysam.xyz>
2024-09-24 14:18:20 -0400

    initial commit

---
+++ src/TraceyGenerator.ts
@@ -0,0 +1,43 @@
+import { customAlphabet } from 'nanoid'
+
+const generateId = customAlphabet('abcdefghijklmnopqrstuv', 16)
+
+type TraecyGeneratorOptions = {
+  sourcePath: string
+  code: string
+  libPath: string
+  hookVariable?: string
+}
+
+export class TraceyGenerator {
+  private options: TraecyGeneratorOptions
+  private debugLibVar: string
+
+  constructor(options: TraecyGeneratorOptions) {
+    this.options = options
+    this.debugLibVar = options.hookVariable ?? generateId()
+  }
+
+  insertTraceHooks() {
+    let foundNonComment = false
+    return this.options.code
+      .split('\n')
+      .map((lineOfCode, lineNumber) => {
+        if (!foundNonComment) {
+          if (lineOfCode.trimStart().startsWith('--')) return lineOfCode
+          foundNonComment = true
+          return `${this.generateTraceImport()} ${lineOfCode}`
+        }
+        return `${this.generateTraceHook(lineNumber + 1)} ${lineOfCode}`
+      })
+      .join('\n')
+  }
+
+  private generateTraceImport() {
+    return `local ${this.debugLibVar} = require(${this.options.libPath})._register('${this.options.sourcePath}')`
+  }
+
+  private generateTraceHook(lineNumber: number) {
+    return `${this.debugLibVar}(${lineNumber})`
+  }
+}
