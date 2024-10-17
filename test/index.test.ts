5c4a6123706b6413d04abeb140a78607b5e3ae38
Wingy <git@wingysam.xyz>
2024-10-16 22:41:42 -0400

    Add additional test case

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,32 +1,47 @@
-import { expect, test } from 'bun:test'
+import { describe, expect, test } from 'bun:test'
 import fs from 'fs/promises'
 
 import { Citronella } from '../src'
 
-import originalCode from './original.luau' with { type: 'text' }
-import expectedCode from './expected.luau' with { type: 'text' }
-import expectedBreakpoints from './expected-breakpoints.json'
-
-test('Generates code properly', async () => {
-  console.log('Original:\n' + originalCode)
-  const transformed = await Citronella.inject({
-    sourcePath: 'test.lua',
-    code: originalCode,
-    libPath: "'@lib/citronella'",
-    hookVariable: '_trace',
-  })
-  console.log('Transformed code:\n' + transformed.code)
-  console.log(
-    'Transformed breakpoints:\n' + JSON.stringify(transformed.breakpoints),
+for (const sample of await fs.readdir('test/samples')) {
+  const originalCode = await fs.readFile(
+    `test/samples/${sample}/original.luau`,
+    'utf-8',
   )
-  if (process.env.WRITE_EXPECTED) {
-    await fs.writeFile('test/expected.luau', transformed.code)
-    await fs.writeFile(
-      'test/expected-breakpoints.json',
-      JSON.stringify(transformed.breakpoints),
-    )
-  } else {
-    expect(transformed.code).toEqual(expectedCode)
-    expect(transformed.breakpoints).toEqual(expectedBreakpoints)
-  }
-})
+  const expectedCode = await fs.readFile(
+    `test/samples/${sample}/expected.luau`,
+    'utf-8',
+  )
+  const expectedBreakpoints = JSON.parse(
+    await fs.readFile(
+      `test/samples/${sample}/expected-breakpoints.json`,
+      'utf-8',
+    ),
+  )
+
+  describe(`Sample: ${sample}`, () => {
+    test('Generates code properly', async () => {
+      console.log('Original:\n' + originalCode)
+      const transformed = await Citronella.inject({
+        sourcePath: 'test.lua',
+        code: originalCode,
+        libPath: "'@lib/citronella'",
+        hookVariable: '_trace',
+      })
+      console.log('Transformed code:\n' + transformed.code)
+      console.log(
+        'Transformed breakpoints:\n' + JSON.stringify(transformed.breakpoints),
+      )
+      if (process.env.WRITE_EXPECTED) {
+        await fs.writeFile('test/expected.luau', transformed.code)
+        await fs.writeFile(
+          'test/expected-breakpoints.json',
+          JSON.stringify(transformed.breakpoints),
+        )
+      } else {
+        expect(transformed.code).toEqual(expectedCode)
+        expect(transformed.breakpoints).toEqual(expectedBreakpoints)
+      }
+    })
+  })
+}
